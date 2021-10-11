03c8b251a7fd849287fe7b0be9abcd9c58d35cd3
Piotr Kozłowski <pitrx77@gmail.com>
2021-10-11 15:22:10 +0200

    update lerna 3
    Trying to fix some problems

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,6 +1,6 @@
 import path from "path";
 import { Command } from "commander";
-import { serve } from "@clijsnote/local-api";
+import { serve } from "@clijsnotex/local-api";
 
 const isProduction = process.env.NODE_ENV === 'production';
 
